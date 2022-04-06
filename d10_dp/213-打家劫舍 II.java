2bc09a182e1498329164e024bbc58ef50705e79a
dushulin <dushulin@xiaomi.com>
2022-04-06 20:09:20 +0800

    ADD: 198 213

---
+++ d10_dp/213-打家劫舍 II.java
@@ -0,0 +1,87 @@
+package stack.leetcode.editor;
+
+//你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的
+//房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [2,3,2]
+//输出：3
+//解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,2,3,1]
+//输出：4
+//解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+//     偷窃到的最高金额 = 1 + 3 = 4 。
+//
+// 示例 3：
+//
+//
+//输入：nums = [1,2,3]
+//输出：3
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 1000
+//
+// Related Topics 数组 动态规划
+// 👍 993 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        if (nums.length == 1){
+            return nums[0];
+        }
+        if (nums.length == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+        //情况一：不考虑开头
+        int result1 = dp_rob(1, nums.length - 1, nums);
+        //情况二：不考虑结尾
+        int result2 = dp_rob(0, nums.length - 2, nums);
+        return Math.max(result1, result2);
+    }
+
+    /**
+     * 考虑[start, end]范围内去偷，可以偷到的最大值
+     * @param start
+     * @param end
+     * @param nums
+     * @return
+     */
+    public int dp_rob(int start, int end, int[] nums){
+        int[] dp = new int[end - start + 2];
+        dp[start] = nums[start];
+        dp[start + 1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++){
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[end];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {0,0};
+        System.out.println(s.rob(nums));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
